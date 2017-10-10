55628e1a388972b227d2e2c8b6e5f48a861d61d8
opussf <github@zz9-za.com>
2017-10-10 03:14:26 -0700

    201701291551

--- src/index.php
+++ src/index.php
@@ -7,16 +7,20 @@ if ($_GET["ms"]) {
 	$ms = intval( $_GET["ms"] );
 }
 if (empty($ms)) { $ms = 800; }
+if ($_GET["width"]) {
+	$width = intval( $_GET["width"]);
+}
+if (empty($width)) { $width= 640; }
 
 
 # build the list of slides
 $img_dir = "imgs";
 
-$all = scandir("./imgs");
+$all = scandir("./$img_dir");
 $afiles = array();
 foreach( $all as $file) {
-	if (!is_dir($file)) {
-		$afiles[] = "imgs/$file";
+	if (!is_dir("./$img_dir/$file")) {
+		$afiles[] = "$img_dir/$file";
 	}
 }
 
@@ -34,6 +38,7 @@ foreach( $alimit as $v ) {
 $alimit[] = $c;
 
 $ams = array(100, 200, 300, 400, 500, 800, 1000, 2000);
+$widths = array(640, 720, 1000, 1440);
 ?>
 <!DOCTYPE html>
 <html lang="en-US">
@@ -42,6 +47,7 @@ $ams = array(100, 200, 300, 400, 500, 800, 1000, 2000);
 <link type='text/css' href="wowshots.css" rel="stylesheet"/>
 </head>
 <body>
+<div class="menus">
 <div class="limit_menu">
 <ul>
 <li>Limit to:</li>
@@ -50,7 +56,7 @@ $ams = array(100, 200, 300, 400, 500, 800, 1000, 2000);
 foreach( $alimit as $a ) {
 	$linkStart = "<strong><mark>"; $linkEnd = "</mark></strong>";
 	if ($a != $limit) {
-		$linkStart = "<a href='index.php?limit=$a&ms=$ms'>";
+		$linkStart = "<a href='index.php?width=$width&limit=$a&ms=$ms'>";
 		$linkEnd = "</a>";
 	}
 	print("<li>$linkStart$a$linkEnd</li>\n");
@@ -65,7 +71,7 @@ foreach( $alimit as $a ) {
 foreach ( $ams as $m ) {
 	$linkStart = "<strong><mark>"; $linkEnd = "</mark></strong>";
 	if ($m != $ms) {
-		$linkStart = "<a href='index.php?limit=$limit&ms=$m'>";
+		$linkStart = "<a href='index.php?width=$width&limit=$limit&ms=$m'>";
 		$linkEnd = "</a>";
 	}
 	$m = $m / 1000;
@@ -75,6 +81,22 @@ foreach ( $ams as $m ) {
 ?>
 </ul>
 </div>
+<div class="width_menu">
+<ul>
+<li>Width:</li>
+<?php
+foreach( $widths as $w ) {
+	$linkStart = "<strong><mark>"; $linkEnd = "</mark></strong>";
+	if ($w != $width) {
+		$linkStart = "<a href='index.php?width=$w&limit=$limit&ms=$ms'>";
+		$linkEnd = "</a>";
+	}
+	print("<li>$linkStart$w$linkEnd</li>\n");
+}
+?>
+</ul>
+</div> <!-- width_menu -->
+</div> <!-- Menus -->
 
 <div class="carousel" style="max-width:1000px">
 <?php
@@ -84,7 +106,11 @@ $afiles = array_slice($afiles, -$limit);
 $lcv = 1; $c = count($afiles);
 foreach( $afiles as $file) {
 	print("<div class=\"mySlides\">");
-	print("<img src=\"$file\" style=\"width:100%\">\n");
+	if ($width < 1440) {
+		print("<img src=\"mythumb.php?fname=$file&w=$width\" style=\"width:100%\">\n");
+	} else {
+		print("<img lowsrc=\"mythumb.php?fname=$file&w=50\" src=\"$file\" style=\"width:100%\">\n");
+	}
 	print("<div class=\"caption\"><a href=\"$file\">$file  $lcv/$c</a></div>");
 	print("</div>");
 	$lcv++;
